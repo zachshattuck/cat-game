f2c7d42489db5c7074b874c1c9efc2df1e5f8e2a
Zach <zdogshattuck@gmail.com>
2021-11-02 23:38:33 -0600

    Canvas set up

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('game')
+const ctx    = canvas.getContext('2d')
+
+//Probably gonna bite the bullet and try making a from-scratch canvas game
+//Probably a waste of time but sounds kinda rewarding + I wanna try it.
