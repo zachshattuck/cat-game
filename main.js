6d36914c155be78394b3db0fdec1257397ec7f5e
Zach <zdogshattuck@gmail.com>
2021-11-04 21:19:07 -0600

    Stack changed to webpack/electron/pixi.js

---
+++ main.js
@@ -0,0 +1,20 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+const createWindow = () => {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    resizable: false,
+    titleBarStyle: 'hidden',
+  })
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'dist/index.html'),
+    protocol: 'file',
+    slashes: true,
+  }))
+}
+
+app.on('ready', createWindow)
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const component = () => {
+  const element = document.createElement('div');
+
+  element.innerHTML = "Hello webpack"
+
+  return element
+}
+
+document.body.appendChild(component())
